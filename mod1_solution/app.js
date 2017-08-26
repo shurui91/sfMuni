a8f08454554b11acaed2a071b0817487230b3e33
Shurui Liu <shurui91@gmail.com>
2017-08-25 17:34:40 -0700

    mod1 code is done

--- mod1_solution/app.js
+++ mod1_solution/app.js
@@ -1,2 +1,56 @@
-// app.js
-var app = angular.module("LunchCheck", []);
+(function () {
+	'use strict';
+	var app = angular.module('LunchCheck',[]);
+	app.controller('LunchCheckController', LunchCheckController);
+	LunchCheckController.$inject = ['$scope'];
+	function LunchCheckController ($scope) {
+		$scope.list = '';
+		$scope.msg = '';
+		$scope.fontStyle = {};
+		$scope.boxStyle = {};
+
+		$scope.checkItems = function () {
+			console.log ('check Items');
+			console.log ($scope.list);
+			if ($scope.list !== '') {
+				var arr = $scope.list.split(',');
+				var count = 0;
+				for (var i = 0; i < arr.length; i++){
+					if (arr[i].trim() !== '') {
+						count++;
+					}
+				}
+				
+				if (count > 0) {
+					if (count <= 3) {
+						$scope.msg = 'Enjoy!';
+					}
+					else if(count > 3) {
+						$scope.msg = 'Too much!'
+					}
+					$scope.fontStyle = {
+						"color":"green"
+					};
+					$scope.boxStyle = {
+						"border-color":"green"
+					};
+				}
+				else {
+					enterItems();
+				}
+			}
+			else {
+				enterItems();
+			}
+		}
+		var enterItems = function() {
+			$scope.msg = 'Please enter your data';
+				$scope.fontStyle = {
+					"color":"red"
+				};
+				$scope.boxStyle = {
+					"border-color":"red"
+				};
+		};
+	}
+})();
